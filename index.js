397a843fd34498999fbe88f9432fc77f4ac9066f
Masihbalali <Masihbalali@outlook.com>
2023-04-30 12:49:13 +0430

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const handleAlert = (e) =>{
+  e.preventDefault();
+  const a = document.getElementById("main-container");
+// a.classList.add("failed-alert");
+a.classList.add("success-alert" );
+
+
+
+}
